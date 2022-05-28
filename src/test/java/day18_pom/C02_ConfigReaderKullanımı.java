116487534cbd7105070a1ee8583e49ec0f5f8081
Gulsen Genc <gulsengenctuncer@gmail>
2022-05-28 23:16:26 +0300

    28.05.2022

---
+++ src/test/java/day18_pom/C02_ConfigReaderKullanımı.java
@@ -0,0 +1,29 @@
+package day18_pom;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C02_ConfigReaderKullanımı {
+    @Test
+    public void test01() {
+
+        // facebook anasayfaya gidin
+        Driver.getDriver().get(ConfigReader.getProperty("facebookUrl"));
+        // cikarsa cookies kabul edin
+
+        // kullanici mail kutusuna rastgele bir isim yazdirin
+        FacebookPage facebookPage = new FacebookPage();
+        facebookPage.emailKutusu.sendKeys(ConfigReader.getProperty("fbWrongUserName"));
+
+        // kullanici sifre kutusuna rastgele bir password yazdirin
+        facebookPage.passwordKutusu.sendKeys(ConfigReader.getProperty("fbWrongPassword"));
+        // login butonuna basin
+        facebookPage.loginButton.click();
+        // giris yapilamadigini test edin
+       Assert.assertTrue(facebookPage.girisYapılamadıYazısı.isDisplayed());
+       Driver.closeDriver();
+    }
+}
